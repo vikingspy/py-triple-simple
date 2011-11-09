18f09ac501764a3ec59df8ec99444985fff2010a
risk.limits <risk.limits@gmail.com>
2011-11-09 19:35:23 +0000

    Added in triple_find functionality

--- src/test/testPyTripleSimple.py
+++ src/test/testPyTripleSimple.py
@@ -22,8 +22,44 @@ class  TestPyTripleSimpleTestCase(unittest.TestCase):
         
         self.assertEquals(30,len(result1),"Wrong number of triples iterated")
         result2 = list(ts.iterator_ntriples())
-        self.assertEquals(30,len(result1),"Wrong number of triples iterated")
-        
+        self.assertEquals(30,len(result2),"Wrong number of triples iterated")
+
+
+    def test_find_triples(self):
+        ts = pyTripleSimple.SimpleTripleStore()
+        ts.load_ntriples(self.test_source)
+
+        r1 = ts.find_triples(subjects="<http://example.org/resource999>")
+        self.assertEquals(set([]),r1,"Should return an empty list")
+
+        r2  = ts.find_triples(subjects="<http://example.org/resource9>")
+        self.assertEquals(1,len(r2))
+
+        r3 = ts.find_triples(predicates="http://example.org/property")
+        self.assertEquals(30,len(r3))
+
+        r4 = ts.find_triples(objects="<http://example.org/resource2>")
+        self.assertEquals(7,len(r4))
+
+        r5 = ts.find_triples(literals="chat")
+        self.assertEquals(3,len(r5))
+
+        r6 = ts.find_triples(subjects=['<http://example.org/resource26>','http://example.org/resource25'])
+        self.assertEquals(3,len(r6))
+
+        r7 = ts.find_triples(subjects=['<http://example.org/resource26>','http://example.org/resource25'], predicates="<http://example.org/property>")
+        self.assertEquals(3,len(r7))
+
+        r8 = ts.find_triples(['<http://example.org/resource26>','http://example.org/resource25'], predicates="<http://example.org/propertyX>")
+        self.assertEquals(0,len(r8))
+
+        r9 = ts.find_triples("<http://example.org/resource14>", "<http://example.org/property>", literals="x")
+        self.assertEquals(1,len(r9))
+
+        r10 = ts.find_triples("<http://example.org/resource14>", "<http://example.org/property>", objects="x")
+        self.assertEquals(0,len(r10))
+
+
 
 if __name__ == '__main__':
     unittest.main()
